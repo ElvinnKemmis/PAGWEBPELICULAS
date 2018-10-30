461b9c12a09337cf7ab9e8d335d0e97a6223965e
ElvinnKemmis <elvin11_99@hotmail.com>
2018-10-30 10:22:58 -0500

    actulizacion

--- registrarse.php
+++ registrarse.php
@@ -1,3 +1,70 @@
-<?php include 'menu/nav.php'?>
-    
-<?php include 'menu/footer.php'?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registro</title>
+</head>
+<body>
+    <?php include 'menu/nav.php'?>
+        <section>
+            <h1 style="text-align:center; font-family: fantasy;"> REGISTRESE</h1>
+            <div id=" tabla_reg">
+            
+                <table>
+                    <tr>
+                        <h2 style=" font-family: fantasy;">DATOS PERSONALES</h2>
+                        <td>
+                           
+                            <label for="">NOMBRE</label>
+                            <input type="text">
+                        </td>
+                        <td>
+                            <label for="">APELLIDOS</label>
+                            <input type="text">
+                        </td>
+                    </tr>
+                </table>
+                <table>
+                    <tr>
+                        <h2 style=" font-family: fantasy;">DATOS ADICIONALES</h2>
+                        <td>
+                            <label for="">TELEFONO</label>
+                            <input type="text">
+                        </td>
+                        <td>
+                            <label for="">DIRECCION</label>
+                            <input type="text">
+                        </td>
+                    </tr>
+                    <tr>
+                        
+                        <td>
+                            <label for="">CORREO</label>
+                            <input type="text">
+                        </td>
+                        
+                    </tr>
+                </table>
+                <table>
+                    <tr>
+                        <h2 style=" font-family: fantasy;">DATOS DE LA PAGINA</h2>
+                        <td>
+                            <label for="">USUARIO</label>
+                            <input type="text">
+                        </td>
+                        <td>
+                            <label for="">CONTRASEÑA</label>
+                            <input type="text">
+                        </td>
+                        
+                    </tr>
+                </table>
+                
+            </div>
+        </section>
+    <?php include 'menu/footer.php'?>
+</body>
+</html>
+
