472a37fdf54ebb801a5f983b7a5e9300bb35dce1
ElvinnKemmis <elvin11_99@hotmail.com>
2018-10-30 10:02:53 -0500

    actualizacion

---
+++ categoriass.php
@@ -0,0 +1,5 @@
+<?php include 'menu/categorias.php'?>
+<?php include 'index.php'?>
+
+
+
--- menu/nav.php
+++ menu/nav.php
@@ -13,7 +13,7 @@
         <ol>
             <li ><img id="img" src="wkYUX_Iq.JPG" style="width: 230px; eight: 320px;" ></li>
             <li><a href="index.php" >INICIO</a> </li>
-            <li><a href="categorias.php" >CATEGORIAS</a> </li>
+            <li><a href="categoriass.php" >CATEGORIAS</a> </li>
             <li><a href="trillers.php" >TRILLERS</a> </li>
             <li><a href="foro.php" >FORO</a></li>
             <li><a href="registrarse.php" >REGISTARSE</a> </li>
