47e683f341ede18caa98b53e11ab3818265dd45d
ElvinnKemmis <elvin11_99@hotmail.com>
2018-10-30 10:40:58 -0500

    actualizacion

--- registrarse.php
+++ registrarse.php
@@ -10,57 +10,64 @@
     <?php include 'menu/nav.php'?>
         <section>
             <h1 style="text-align:center; font-family: fantasy;"> REGISTRESE</h1>
-            <div id=" tabla_reg">
-            
-                <table>
-                    <tr>
-                        <h2 style=" font-family: fantasy;">DATOS PERSONALES</h2>
-                        <td>
-                           
-                            <label for="">NOMBRE</label>
-                            <input type="text">
-                        </td>
-                        <td>
-                            <label for="">APELLIDOS</label>
-                            <input type="text">
-                        </td>
-                    </tr>
-                </table>
-                <table>
-                    <tr>
-                        <h2 style=" font-family: fantasy;">DATOS ADICIONALES</h2>
-                        <td>
-                            <label for="">TELEFONO</label>
-                            <input type="text">
-                        </td>
-                        <td>
-                            <label for="">DIRECCION</label>
-                            <input type="text">
-                        </td>
-                    </tr>
-                    <tr>
-                        
-                        <td>
-                            <label for="">CORREO</label>
-                            <input type="text">
-                        </td>
-                        
-                    </tr>
-                </table>
-                <table>
-                    <tr>
-                        <h2 style=" font-family: fantasy;">DATOS DE LA PAGINA</h2>
-                        <td>
-                            <label for="">USUARIO</label>
-                            <input type="text">
-                        </td>
-                        <td>
-                            <label for="">CONTRASEÑA</label>
-                            <input type="text">
-                        </td>
-                        
-                    </tr>
-                </table>
+            <div >
+                <form  id="tabla" action="validacion_registro.php" method="post">
+                    <table>
+                        <tr>
+                            <h2 style=" font-family: fantasy;">DATOS PERSONALES</h2>
+                            <td>
+                            
+                                <label for="">NOMBRE</label>
+                                <input type="text" name="nombre">
+                            </td>
+                            <td>
+                                <label for="">APELLIDOS</label>
+                                <input type="text" name="apellido">
+                            </td>
+                        </tr>
+                    </table>
+                    <table>
+                        <tr>
+                            <h2 style=" font-family: fantasy;">DATOS ADICIONALES</h2>
+                            <td>
+                                <label for="">TELEFONO</label>
+                                <input type="...." name="telefono"> 
+                            </td>
+                            <td>
+                                <label for="">DIRECCION</label>
+                                <input type="text" name="direccion">
+                            </td>
+                        </tr>
+                        <tr>
+                            
+                            <td>
+                                <label for="">CORREO</label>
+                                <input type="email" name="correo">
+                            </td>
+                            
+                        </tr>
+                    </table>
+                    <table>
+                        <tr>
+                            <h2 style=" font-family: fantasy;">DATOS DE LA PAGINA</h2>
+                            <td>
+                                <label for="">USUARIO</label>
+                                <input type="text">
+                            </td>
+                            <td>
+                                <label for="">CONTRASEÑA</label>
+                                <input type="text">
+                            </td>
+                            
+                        </tr>
+                    </table>
+                    <div id="bot">
+                        <button type="submit">REGISTRARSE</button>
+                    
+                    </div>
+                </form>
+                
+               
                 
             </div>
         </section>
---
+++ validacion_registro.php
@@ -0,0 +1,3 @@
+<?php include 'menu/nav.php'?>
+    <h1>REGISTRO EXITOSO</h1>
+<?php include 'menu/footer.php'?>
