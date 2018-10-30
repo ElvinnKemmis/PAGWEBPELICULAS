2391f3d1857b95309962e6ceba8c467a0261ff17
ElvinnKemmis <elvin11_99@hotmail.com>
2018-10-30 10:44:08 -0500

    actulizacion

---
+++ iniciar_seccion.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>INICIAR_SECCION</title>
+</head>
+<body>
+<table>
+    <tr>
+        <h2 style=" font-family: fantasy;">DATOS DE LA PAGINA</h2>
+            <td>
+                <label for="">USUARIO</label>
+                <input type="text">
+            </td>
+            <td>
+                <label for="">CONTRASEÑA</label>
+                <input type="text">
+            </td>
+                            
+    </tr>
+</table>
+</body>
+</html>
--- registrarse.php
+++ registrarse.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Registro</title>
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
     <?php include 'menu/nav.php'?>
@@ -66,9 +67,7 @@
                     
                     </div>
                 </form>
-                
-               
-                
+
             </div>
         </section>
     <?php include 'menu/footer.php'?>
