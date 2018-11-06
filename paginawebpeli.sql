460517faff4c265f8f67e4899ba429e0ccaf1e60
Elvinnkemmis <elvin11_99@hotmail.com>
2018-11-06 10:47:41 -0500

    actualizacion.

--- paginawebpeli.sql
+++ paginawebpeli.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-10-2018 a las 16:51:20
+-- Tiempo de generación: 06-11-2018 a las 16:45:11
 -- Versión del servidor: 10.1.25-MariaDB
 -- Versión de PHP: 7.1.7
 
@@ -25,6 +25,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `comentario`
+--
+
+CREATE TABLE `comentario` (
+  `id` int(11) NOT NULL,
+  `comenta` varchar(600) NOT NULL,
+  `usuario` varchar(20) NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `comentario`
+--
+
+INSERT INTO `comentario` (`id`, `comenta`, `usuario`, `fecha`) VALUES
+(7, 'qwe', 'a1cczz', '2018-11-06 10:42:02');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -36,7 +56,7 @@ CREATE TABLE `usuarios` (
   `dirrecion` varchar(100) NOT NULL,
   `correo` varchar(20) NOT NULL,
   `usuario` varchar(10) NOT NULL,
-  `contraseña` varchar(10) NOT NULL
+  `contraseña` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -44,11 +64,17 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `telefono`, `dirrecion`, `correo`, `usuario`, `contraseña`) VALUES
-(12, 'Elvinn Kemmis', 'Vasquez Chavez', '921775199', 'JR:asd', '', 'a1cczz', '123');
+(15, 'Elvinn Kemmis', 'vásquez Chávez', '921775195', 'JR.30 setiembre', 'elvin11-99@hotmail.c', 'a1cczz', '40bd001563085fc35165329ea1ff5c5ecbdbbeef');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -61,10 +87,15 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;COMMIT;
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
