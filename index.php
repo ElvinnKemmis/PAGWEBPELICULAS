2b9cc924e1be1a1b513f336d2f5b2b96c23c9179
ElvinnKemmis <elvin11_99@hotmail.com>
2018-11-09 23:51:31 -0500

    Actulizacion

--- index.php
+++ index.php
@@ -9,11 +9,37 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>INDEX</title>
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
     <?php include 'menu/nav.php'?>
-    <section>
-        <h1>kjfl</h1>
+    <section >
+        <div id="portadas">
+
+            <div >
+                <a href="https://repelis.live/11/monster-party-2018/"><img src="https://image.tmdb.org/t/p/w154/db6f237Ed3b8uigSzyRXpF4GxvP.jpg" alt=""></a>
+            </div>
+
+            <div >
+                <a href=""><img src="https://image.tmdb.org/t/p/w154/4nKoB6wMVXfsYgRZK5lHZ5VMQ6J.jpg" alt=""></a>
+            </div>
+
+            <div >
+                <a href="https://repelis.live/11/monster-party-2018/"><img src="https://image.tmdb.org/t/p/w154/db6f237Ed3b8uigSzyRXpF4GxvP.jpg" alt=""></a>
+            </div>
+
+            <div>
+                <a href=""><img src="https://image.tmdb.org/t/p/w154/4nKoB6wMVXfsYgRZK5lHZ5VMQ6J.jpg" alt=""></a>
+            </div>
+            <div >
+                <a href="https://repelis.live/11/monster-party-2018/"><img src="https://image.tmdb.org/t/p/w154/db6f237Ed3b8uigSzyRXpF4GxvP.jpg" alt=""></a>
+            </div>
+
+            <div >
+                <a href=""><img src="https://image.tmdb.org/t/p/w154/4nKoB6wMVXfsYgRZK5lHZ5VMQ6J.jpg" alt=""></a>
+            </div>
+         
+        </div>
     </section>
     <?php include 'menu/footer.php'?>
 </body>
---
+++ visualizacion.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>visualiacion</title>
+</head>
+<body>
+    <?php include 'menu/nav.php'?>
+    <section>
+        <div><img src="https://www.youtube.com/watch?v=XKU7EVIRw7s" alt="">EL REJOJ E LA PARED</div>
+    </section>
+    <?php include 'menu/footer.php'?>
+
+</body>
+</html>
