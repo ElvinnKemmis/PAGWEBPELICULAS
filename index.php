cad7f078465829826305637967f656188aad330b
ElvinnKemmis <elvin11_99@hotmail.com>
2018-11-10 00:04:00 -0500

    Actualizacionn

--- index.php
+++ index.php
@@ -14,28 +14,29 @@ session_start();
 <body>
     <?php include 'menu/nav.php'?>
     <section >
-        <div id="cont">
+        <div style="display:flex;">
 
-            <div >
+            <div id="poster">
                 <a href="https://repelis.live/11/monster-party-2018/"><img src="https://image.tmdb.org/t/p/w154/db6f237Ed3b8uigSzyRXpF4GxvP.jpg" alt=""></a>
-            </div>
+                
+            </div id="poster">
 
-            <div >
+            <div id="poster">
                 <a href=""><img src="https://image.tmdb.org/t/p/w154/4nKoB6wMVXfsYgRZK5lHZ5VMQ6J.jpg" alt=""></a>
             </div>
 
-            <div >
+            <div id="poster">
                 <a href="https://repelis.live/11/monster-party-2018/"><img src="https://image.tmdb.org/t/p/w154/db6f237Ed3b8uigSzyRXpF4GxvP.jpg" alt=""></a>
             </div>
 
-            <div>
+            <div id="poster">
                 <a href=""><img src="https://image.tmdb.org/t/p/w154/4nKoB6wMVXfsYgRZK5lHZ5VMQ6J.jpg" alt=""></a>
             </div>
-            <div >
+            <div id="poster">
                 <a href="https://repelis.live/11/monster-party-2018/"><img src="https://image.tmdb.org/t/p/w154/db6f237Ed3b8uigSzyRXpF4GxvP.jpg" alt=""></a>
             </div>
 
-            <div >
+            <div id="poster">
                 <a href=""><img src="https://image.tmdb.org/t/p/w154/4nKoB6wMVXfsYgRZK5lHZ5VMQ6J.jpg" alt=""></a>
             </div>
          
