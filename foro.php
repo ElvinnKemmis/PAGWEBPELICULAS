2f2ebcc3cc5818f419d91b634314c4a94ac6fc54
ElvinnKemmis <elvin11_99@hotmail.com>
2018-11-11 11:11:28 -0500

    foro terminado

--- foro.php
+++ foro.php
@@ -15,70 +15,76 @@ $img=$stmt->fetchAll();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
     <?php include 'menu/nav.php'?>
+
+    <h1 style="font-family: fantasy; text-align:center;">SECCION FORO</h1>
+    <p style="font-family: fantasy; font-size: 25px;">Escriba en la caja de comentarios para poder comentar:</p>
+    
+    <div style="background-color:black; color:white; padding:15px; text-align:right;">
+        <form action="procesar_imagen.php" method="post" enctype="multipart/form-data">
+            <label >SI DESEA SUBIR UNA IMAGEN POR FAVOR SELECIONE LA IMAGEN EN FORMATO JPG</label>
+            <input type="file" name="imagen" >
+            <button type="submit">PUBLICAR</button>
+        </form>
+    
+    </div>
     
     <form   action="procesar_foro.php" method="post">
         <table > 
             <tr>
                 <td><textarea name="comentario" rows="6" cols="200"></textarea></td>
-                
             </tr>
-            <td><button type="submit">COMENTAR</button></td>
+                <td><button type="submit">COMENTAR</button></td>
         </table>
         
     </form>
+    </br>
+    </br>
+    <section style="display: flex; " id="img_conte" >
+        <div id="imge" >
+            <h1 style="font-family: fantasy; text-align:center;">IMAGENES</h1>
 
-    <form action="procesar_imagen.php" method="post" enctype="multipart/form-data">
-        <label >SELECIONE IMAGEN A SUBIR</label>
-        <input type="file" name="imagen" >
-        <button type="submit">ENVIAR</button>
-    </form>
+            <?php foreach($img as $i){ ?>
+                <div style="margin:15px;">
 
-    <?php foreach($img as $i){ ?>
-        <table>
-            <tr>
-                <td><img src="data:image/jpg;base64, <?php echo base64_encode($i["imagen"]); ?>"> </td>
-                <td> <p  style="font-family: fantasi; font-size: 12px; color: blue;"><?php echo $i["usuario"] ?></p></td>
-                <td><p style="font-family: fantasi; font-size: 12px; color: blue;"><?php echo $i["fecha"] ?></p></td>
+                    <div style="margin-left:35%;">       
+                        <td><img style="width: 50%; " src="data:image/jpg;base64, <?php echo base64_encode($i["imagen"]); ?>"> </br>
+                    </div> 
+                            <p  style="font-family: fantasi; font-size: 12px; text-align:center;">Publicado por <?php echo  $i["usuario"]?> el <?php echo $i["fecha"] ?> </p>
+                    <div style="margin-left:45%;">
+                            <form action="borrar_imagen.php" method="post">
+                                <?php if($_SESSION["usuario"] == $i["usuario"]) {?>
+                                    <input type="hidden" name="id" value="<?php echo $i["id"] ?>">
+                                    <button type="submit">ELIMINAR IMAGEN</button></td>
+                                <?php }?>
+                            </form>
+                    </div>
+                </div>
+            <?php }?>    
+        </div>
+                     
+            
+        <div id="conte">    
+               <h1 style="font-family: fantasy; text-align:center">COMENTARIOS</h1>
+                <?php foreach($comentario as $c){ ?>
+                        <div style="border: 2px solid black; padding: 6px;margin:9px;">
+                               <strong><p><?php echo $c["comenta"] ?></p></strong>
+                                <p  style="font-family: fantasi; font-size: 12px; color: blue;">Publicado por <?php echo $c["usuario"] ?> el <?php echo $c["fecha"] ?></p>
 
-                <form action="borrar_imagen.php" method="post">
-                    <?php if($_SESSION["usuario"] == $i["usuario"]) {?>
-                        <input type="hidden" name="id" value="<?php echo $i["id"] ?>">
-                        <td><button type="submit">ELIMINAR IMAGEN</button></td>
-                    <?php }?>
-                </form>
-
-            </tr>
+                                <form action="borrar_comentario.php" method="post">
+                                    <?php if($_SESSION["usuario"] == $c["usuario"]) {?>
+                                        <input type="hidden" name="id" value="<?php echo $c["id"] ?>">
+                                        <button type="submit">ELIMINAR COMENTARIO</button>
+                                    <?php }?>
+                                </form>
+                        </div>
+                <?php }?>
         
-        </table>
-
-    <?php }?>
-
-
-
-    
-    <?php foreach($comentario as $c){ ?>
-        <table>
-            <tr>
-                <td> <p><?php echo $c["comenta"] ?></p></td>
-                <td>  <p  style="font-family: fantasi; font-size: 12px; color: blue;"><?php echo $c["usuario"] ?></p></td>
-                <td><p style="font-family: fantasi; font-size: 12px; color: blue;"><?php echo $c["fecha"] ?></p></td>
-
-                <form action="borrar_comentario.php" method="post">
-                    <?php if($_SESSION["usuario"] == $c["usuario"]) {?>
-                        <input type="hidden" name="id" value="<?php echo $c["id"] ?>">
-                        <td><button type="submit">ELIMINAR COMENTARIO</button></td>
-                    <?php }?>
-                </form>
-
-            </tr>
-        
-        </table>
-        <?php }?>
-    
-   
+        </div>
+    </section>
     <?php include 'menu/footer.php'?>
 </body>
 </html>
