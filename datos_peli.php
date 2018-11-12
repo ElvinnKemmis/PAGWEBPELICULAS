76fd6639986a3cfc34e5739a1bb8737b56da9256
ElvinnKemmis <elvin11_99@hotmail.com>
2018-11-11 20:47:58 -0500

    terminado v0.2

--- datos_peli.php
+++ datos_peli.php
@@ -3,7 +3,7 @@ session_start();
 
 $i= addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
 $t=$_POST['titulo'];
-$d=$_POST['duracion'];
+$du=$_POST['duracion'];
 $e=$_POST['estreno'];
 $g=$_POST['genero'];
 $tr=$_POST['trailer'];
@@ -13,7 +13,7 @@ $u=$_SESSION['usuario'];
 
 
 $db = new PDO('mysql:host=localhost;dbname=paginawebpeli; charset=utf8mb4', 'root', ''); 
-$stmt=$db->query("INSERT INTO peliculas VALUES(NULL,'$t','$i','$d','$e','$g','$tr','$u',SYSDATE(),'$d','$p')");
+$stmt=$db->query("INSERT INTO peliculas VALUES(NULL,'$t','$i','$du','$e','$g','$tr','$u',SYSDATE(),'$d','$p')");
 
 
 session_start();
